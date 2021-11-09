0b6c8f3e8efd310cd58e8f71120d821620df6a06
YoniqueeZyzzFan <lolvan@mail.ru>
2021-11-09 17:33:34 +0400

    key_generator is done

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from cryptography.hazmat.primitives.ciphers.algorithms import Blowfish
 from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.asymmetric import padding
 import os
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 # Симметричный алгоритм - Blowfish / Вариант 8
@@ -21,7 +23,9 @@ def input_length() -> int:
 
 
 def key_generator(encrypted_symmetrical_key_path: str, public_key_path: str, private_key_path: str):
-    symmetrical_key = Blowfish(os.urandom(input_length()))  # Симметричный ключ
+    x = os.urandom(input_length())
+    print(type(x))
+    symmetrical_key = algorithms.Blowfish(x)  # Симметричный ключ
     rsa_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     private_key = rsa_key  # Ассиметричный приватный ключ
     public_key = rsa_key.public_key()  # Ассиметричный публичный ключ
@@ -38,7 +42,7 @@ def key_generator(encrypted_symmetrical_key_path: str, public_key_path: str, pri
                                                     encryption_algorithm=serialization.NoEncryption()))
     # шифрование симметричного ключа при помощи RSA-OAEP
     encrypt_symmetrical_key = public_key.encrypt(
-        symmetrical_key,
+        symmetrical_key.key,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
@@ -46,12 +50,13 @@ def key_generator(encrypted_symmetrical_key_path: str, public_key_path: str, pri
         )
     )
     # сериализация шифрованного симметричного ключа в файл
-    with open(encrypted_symmetrical_key_path, 'wb') as key_file:
+    with open(encrypted_symmetrical_key_path + '\\encrypted_symmetrical.pem', 'wb') as key_file:
         key_file.write(encrypt_symmetrical_key)
+        """
     # Расшифровка ключа симм алгоритма
-    with open(encrypted_symmetrical_key_path, 'rb') as key_file:
+    with open(encrypted_symmetrical_key_path + '\\encrypted_symmetrical.pem', 'rb') as key_file:
         ciphertext = key_file.read()
-    decrypt_symmetrical_key= private_key.decrypt(
+    decrypt_symmetrical_key = private_key.decrypt(
         ciphertext,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -59,9 +64,17 @@ def key_generator(encrypted_symmetrical_key_path: str, public_key_path: str, pri
             label=None
         )
     )
-    if decrypt_symmetrical_key == symmetrical_key:
+    if decrypt_symmetrical_key == symmetrical_key.key:
         print('Its working, wooohoooo!!!!!! ')
+        """
 
+
+def encrypt_text_file(path_to_text: str, private_key_path: str, encrypted_symmetrical_key_path: str, save_to_path: str):
+    with open(encrypted_symmetrical_key_path, 'rb') as key_file:
+        encrypted_symmetrical_key = key_file.read()
+    with open(private_key_path, '')
 
 
 if __name__ == '__main__':
+    str1 = 'G:\\InfromationSecurityLabs\\lab3'
+    key_generator(str1, str1, str1)
